930a0a9b368a07f31dd0aa18eeae64d76b3349aa
KUSH23 <kushalsai2007@gmail.com>
2022-02-24 23:46:32 +0530

    further

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,21 @@
 <script>
-	export let name;
+  // export let name;
+  import Navbar from "./components/navbar.svelte";
+  import expenseData from "./data/expenses";
+  import Title from "./components/title.svelte";
+
+  // let fruits = ["apple", "orange", "lemon"];
+  let expenses = [...expenseData];
+  console.log(expenses);
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Navbar />
+
+<ul>
+  {#each expenses as item}
+    <Title title={item.name} />
+  {/each}
+</ul>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ src/components/navbar.svelte
@@ -0,0 +1,9 @@
+<nav class="nav">
+    <div class="nav-center">
+        <h1 class="nav-title">Calculator</h1>
+        <button type="button" class="nav-btn">
+            <i class="far fa-plus-square" />
+            Add item
+        </button>
+    </div>
+</nav>
